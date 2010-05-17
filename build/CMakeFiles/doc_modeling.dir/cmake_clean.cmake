446c2ec1742da39dca90d8336209b41bc6689e37
Pierre Casadebaig <pierre.casadebaig@toulouse.inra.fr>
2010-05-17 15:46:35 +0200

    Deleted build dir

--- build/CMakeFiles/doc_modeling.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/doc_modeling"
-  "doxygen/modeling/index.html"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/doc_modeling.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build/CMakeFiles/doc_sources.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/doc_sources"
-  "doxygen/sources/index.html"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/doc_sources.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
